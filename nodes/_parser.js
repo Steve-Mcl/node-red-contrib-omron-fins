c44950e98a9bcb03a786ad7c49fd1a56d41f7da9
Steve-Mcl <sdmclaughlin@gmail.com>
2021-06-29 16:04:09 +0100

    fix distinction between C/T bit and TIM/CNT WD

--- nodes/_parser.js
+++ nodes/_parser.js
@@ -13,15 +13,23 @@ function kvMaker(fnAddressToString, address, values) {
 
 function kvMakerBits(fnAddressToString, address, values, asBool) {
     let kvs = {};
+    let tc = false;
+    if(address.MemoryArea == "C" || address.MemoryArea == "T") {
+        tc = true;
+    }
     if (values) {
         let iWD = 0;
         let iBit = 0;
         for (let x in values) {
             let item_addr = fnAddressToString(address, iWD, iBit);
             kvs[item_addr] = asBool ? !!values[x] : values[x];
-            iBit++;
-            if (address.Bit + iBit > 15) {
-                iBit = -address.Bit;
+            if(!tc) {
+                iBit++;
+                if (address.Bit + iBit > 15) {
+                    iBit = -address.Bit;
+                    iWD++;
+                }
+            } else {
                 iWD++;
             }
         }
