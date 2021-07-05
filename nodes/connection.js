809e401c1cf182c55e34b96704d792833691bbaa
Steve-Mcl <sdmclaughlin@gmail.com>
2021-07-05 19:09:01 +0100

    change mode to typedInput to permit env value - clarify this in built-in help - change grouped MODES CSCJ and NJNX into individual choices

--- nodes/connection.js
+++ nodes/connection.js
@@ -22,72 +22,83 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-var {FinsConstants: constants, FinsDataUtils: {isInt}} = require('omron-fins');
-
-/*!
- * Get value of environment variable.
- * @param {RED} _RED - accessing RED object
- * @param {String} name - name of variable
- * @return {String} value of env var / setting
- */
-function getSetting(_RED, name) {
-    var result = _RED.util.getObjectProperty(_RED.settings, name);
-    return result || process.env[name];
-}
-
-/**
- * Checks if a String contains any Environment Variable specifiers and returns
- * it with their values substituted in place.
- *
- * For example, if the env var `WHO` is set to `Joe`, the string `Hello ${WHO}!`
- * will return `Hello Joe!`.
- * @param  {String} value - the string to parse
- * @param  {Node} node - the node evaluating the property
- * @return {String} The parsed string
- */
-function resolveSetting(value, RED) {
-    try {
-        if (!value) return value;
-        if (typeof value != "string") return value;
-        var result;
-        if (/^\${[^}]+}$/.test(value)) {
-            // ${ENV_VAR}
-            var name = value.substring(2, value.length - 1);
-            result = getSetting(RED, name);
-        } else {
-            // FOO${ENV_VAR}BAR
-            result = value.replace(/\${([^}]+)}/g, function (match, name) {
-                return getSetting(RED, name);
-            });
-        }
-        return (result == null) ? value : result;
-    } catch (error) {
-        return value;
-    }
-
-}
+const {FinsConstants: constants, FinsDataUtils: {isInt}} = require('omron-fins');
+const connection_pool = require('../connection_pool.js');
 
 module.exports = function (RED) {
-    const connection_pool = require('../connection_pool.js');
+
+    /*!
+    * Get value of environment variable.
+    * @param {RED} _RED - accessing RED object
+    * @param {String} name - name of variable
+    * @return {String} value of env var / setting
+    */
+    function getSetting(name) {
+        let result = RED.util.getObjectProperty(RED.settings, name);
+        return result || process.env[name];
+    }
+
+    /**
+     * Checks if a String contains any Environment Variable specifiers and returns
+     * it with their values substituted in place.
+     *
+     * For example, if the env var `WHO` is set to `Joe`, the string `Hello ${WHO}!`
+     * will return `Hello Joe!`.
+     * @param  {String} value - the string to parse
+     * @param  {Node} node - the node evaluating the property
+     * @return {String} The parsed string
+     */
+    function resolveSetting(value) {
+        try {
+            if (!value) return value;
+            if (typeof value != "string") return value;
+            let result;
+            if (/^\${[^}]+}$/.test(value)) {
+                // ${ENV_VAR}
+                let name = value.substring(2, value.length - 1);
+                result = getSetting(name);
+            } else {
+                // FOO${ENV_VAR}BAR
+                result = value.replace(/\${([^}]+)}/g, function (match, name) {
+                    return getSetting(name);
+                });
+            }
+            return (result == null) ? value : result;
+        } catch (error) {
+            return value;
+        }
+    }
+
     function omronConnection(config) {
         RED.nodes.createNode(this, config);
         this.name = config.name;
-        this.host = resolveSetting(config.host, RED);
-        this.port = resolveSetting(config.port, RED);
+        this.host = resolveSetting(config.host);
+        this.port = resolveSetting(config.port);
         this.options = {};
+        this.options.MODE = 'CJ';
+        this.options.protocol = 'udp';
         if (config.protocolType == "env") {
-            this.options.protocol = resolveSetting(config.protocol, RED);
+            this.options.protocol = '';
+            if(config.protocol) this.options.protocol = getSetting(config.protocol);
         } else {
             this.options.protocol = config.protocolType || "udp";
         }
-        this.options.MODE = config.MODE ? config.MODE : "CSCJ";
-        this.options.ICF = isInt(resolveSetting(config.ICF, RED), constants.DefaultFinsHeader.ICF);
-        this.options.DNA = isInt(resolveSetting(config.DNA, RED), constants.DefaultFinsHeader.DNA);
-        this.options.DA1 = isInt(resolveSetting(config.DA1, RED), constants.DefaultFinsHeader.DA1);
-        this.options.DA2 = isInt(resolveSetting(config.DA2, RED), constants.DefaultFinsHeader.DA2);
-        this.options.SNA = isInt(resolveSetting(config.SNA, RED), constants.DefaultFinsHeader.SNA);
-        this.options.SA1 = isInt(resolveSetting(config.SA1, RED), constants.DefaultFinsHeader.SA1);
-        this.options.SA2 = isInt(resolveSetting(config.SA2, RED), constants.DefaultFinsHeader.SA2);
+        if(!config.MODEType && (config.MODE == 'CSCJ' || config.MODE == 'NJNX' || config.MODE == 'CV')) {
+            config.MODEType = config.MODE.substr(0,2);
+        }
+        if (config.MODEType == 'env') {
+            if(config.MODE) this.options.MODE = getSetting(config.MODE);
+        } else {
+            this.options.MODE = config.MODEType || 'CJ';
+        }
+        if(this.options.MODE) this.options.MODE = this.options.MODE.substr(0,2);
+        this.options.ICF = isInt(resolveSetting(config.ICF), constants.DefaultFinsHeader.ICF);
+        this.options.DNA = isInt(resolveSetting(config.DNA), constants.DefaultFinsHeader.DNA);
+        this.options.DA1 = isInt(resolveSetting(config.DA1), constants.DefaultFinsHeader.DA1);
+        this.options.DA2 = isInt(resolveSetting(config.DA2), constants.DefaultFinsHeader.DA2);
+        this.options.SNA = isInt(resolveSetting(config.SNA), constants.DefaultFinsHeader.SNA);
+        this.options.SA1 = isInt(resolveSetting(config.SA1), constants.DefaultFinsHeader.SA1);
+        this.options.SA2 = isInt(resolveSetting(config.SA2), constants.DefaultFinsHeader.SA2);
         this.autoConnect = config.autoConnect == null ? true : config.autoConnect;
 
         // eslint-disable-next-line no-unused-vars
